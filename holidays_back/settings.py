ee9897120c96dfe9d7d0e39f8f30f0c416c83ad3
Vadim Fabrichnov <zorumnipa@gmail.com>
2019-02-18 12:02:51 +0300

    heroku setup

--- holidays_back/settings.py
+++ holidays_back/settings.py
@@ -125,6 +125,11 @@ EMAIL_HOST_USER = None
 EMAIL_HOST_PASSWORD = None
 EMAIL_PORT = None
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 try:
     from .local_settings import *
 except ImportError:
